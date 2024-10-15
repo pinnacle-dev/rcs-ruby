412129ddd39713c3643dcb93a05d5e86f62cf98d
ivan <40708241+aivanzhang@users.noreply.github.com>
2024-10-14 23:01:26 -0700

    Update gemconfig.rb

--- lib/gemconfig.rb
+++ lib/gemconfig.rb
@@ -4,9 +4,9 @@ module Pinnacle
   module Gemconfig
     VERSION = ""
     AUTHORS = [""].freeze
-    EMAIL = ""
-    SUMMARY = ""
-    DESCRIPTION = ""
+    EMAIL = "founders@trypinnacle.app"
+    SUMMARY = "The Rich Communication Services API for Developers"
+    DESCRIPTION = "The Rich Communication Services API for Developers"
     HOMEPAGE = "https://github.com/pinnacle-dev/rcs-ruby"
     SOURCE_CODE_URI = "https://github.com/pinnacle-dev/rcs-ruby"
     CHANGELOG_URI = "https://github.com/pinnacle-dev/rcs-ruby/blob/master/CHANGELOG.md"
